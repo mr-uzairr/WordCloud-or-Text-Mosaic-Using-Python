6a7e7be93ec9e73a30fc064d7933e31235aa8372
Uzair <uzairuzair050@gmail.com>
2025-04-01 22:27:11 +0500

    Extract content of the topic

--- word.py
+++ word.py
@@ -7,3 +7,7 @@ from wordcloud import wordCloud,STOPWORDS
 
 a = str(input("Enter the name of word which you want to make word cloud = "))
 title = wikipedia.search(a)[0] # it will search the title from wikipedia
+
+page = wikipedia.page(title)
+text = page.content # To extract the content of that topic
+
