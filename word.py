e0e64d406c33ad3792dc9e4da8397455a2b97ad8
Uzair <uzairuzair050@gmail.com>
2025-04-01 22:25:10 +0500

    Search title from wikipedia

---
+++ word.py
@@ -0,0 +1,9 @@
+import sys
+import numpy as np
+from PIL import Image
+import wikipedia
+from wordcloud import wordCloud,STOPWORDS
+#use STOPWORDS to remove common words like 'the' 'a' etc
+
+a = str(input("Enter the name of word which you want to make word cloud = "))
+title = wikipedia.search(a)[0] # it will search the title from wikipedia
